065be0a4c07a7c4ae62808a88b4aa86f9247ff8d
MarkuB2 <MarkuB2@CSTRL0051186877.destr.corpintra.net>
2018-08-15 14:04:45 +0200

    Technischer Durchstich PUT Request erfolgreich!

--- iPeopleBackend/src/main/java/application/IPeopleKIM.java
+++ iPeopleBackend/src/main/java/application/IPeopleKIM.java
@@ -4,15 +4,35 @@ public class IPeopleKIM {
 	private String kim;
 	private String vorname;
 	private String nachname;
-	
-	IPeopleKIM(String kim){
-		this.kim = kim;	
-		// this is for testing
-		this.vorname = "MMarkus";
-		this.nachname = "BBrand";
+
+	IPeopleKIM() {
 	}
-	
+
+	IPeopleKIM(String kim) {
+		this.kim = kim;
+	}
+
 	public String getKIM() {
 		return kim;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setKIM(String kim) {
+		this.kim = kim;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
 	}
 }
--- iPeopleBackend/src/main/java/application/IPeopleKIMController.java
+++ iPeopleBackend/src/main/java/application/IPeopleKIMController.java
@@ -1,6 +1,11 @@
 package application;
 
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IPeopleKIMController {
 	
+	private static final Logger log = Logger.getLogger( IPeopleKIMController.class.getName() );
+	
 	@RequestMapping(value = "/IPeopleKIM/{kim}", method = RequestMethod.GET)
-	/*public void TestSendRequest(@PathVariable("kim") String kim) {
-
-	}*/
 	public String TestReturn(@PathVariable("kim") String kim) {
 		RequestSender RS = new RequestSender(kim);
 		RS.PutKIM();
 		return kim;
 	}
+	/*
+	@CrossOrigin(origins = "http://localhost:8092")
+	@RequestMapping(value = "/IPeopleKIM/{kim}", method = RequestMethod.PUT, @RequestBody IPeopleKIM inputKIM)
+	public void PutKIM() {
+		
+	}*/
+	
+	@CrossOrigin(origins = "http://localhost:8092")
+	@PutMapping("/IPeopleKIM/{kim}")
+	public void PutKIM(@PathVariable String kim, @RequestBody IPeopleKIM input) {
+		log.info("PUT REQUEST RECEIVED: Nachname: " + input.getNachname() + " , Vorname: " + input.getVorname());
+	}
+	
 }
